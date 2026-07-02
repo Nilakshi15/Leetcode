0840a3a3fd1db14781bff02e6c3684151e17a2ba
Nilakshi Patil <patilnilakshi35@gmail.com>
2026-07-02 19:51:17 +0530

    Time: 2 ms (70.04%), Space: 11 MB (90.22%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        string st;
+
+        for (char digit : num) {
+
+            // Remove larger previous digits
+            while (!st.empty() && k > 0 && st.back() > digit) {
+
+                st.pop_back();
+                k--;
+            }
+
+            st.push_back(digit);
+        }
+
+        // Remove remaining digits from the end
+        while (k > 0 && !st.empty()) {
+            st.pop_back();
+            k--;
+        }
+
+        // Remove leading zeros
+        int i = 0;
+        while (i < st.size() && st[i] == '0')
+            i++;
+
+        string ans = st.substr(i);
+
+        // If empty, return 0
+        return ans.empty() ? "0" : ans;
+    }
+};
