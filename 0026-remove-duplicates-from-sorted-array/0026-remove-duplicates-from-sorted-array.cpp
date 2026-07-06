731b0b2aaf666c4ef3fc8ef7cecff50fb26ecc11
Nilakshi Patil <patilnilakshi35@gmail.com>
2026-07-06 22:05:52 +0530

    Time: 0 ms (100%), Space: 22.7 MB (52.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -3,15 +3,15 @@ public:
     int removeDuplicates(vector<int>& nums) {
        if (nums.empty()) return 0;
 
-    int i = 0; // slow pointer for unique values
-    for (int j = 1; j < nums.size(); ++j) {
-        
-        if (nums[j] != nums[i]) {
-            ++i;
-            nums[i] = nums[j]; // overwrite with unique element
-        }
-    }
+        int i = 0; // slow pointer for unique values
+        for (int j = 1; j < nums.size(); ++j) {
 
-    return i + 1; 
+            if (nums[j] != nums[i]) {
+                ++i;
+                nums[i] = nums[j]; // overwrite with unique element
+            }
+        }
+
+        return i + 1; 
     }
 };
