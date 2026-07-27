60c74895e6376ebb8578ed2bebb3b25fd78f1362
Nilakshi Patil <patilnilakshi35@gmail.com>
2026-07-27 15:06:27 +0530

    Time: 2 ms (15.51%), Space: 17.8 MB (23.93%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = nums[0];
+
+        // Start from the second element
+        for (int i = 1; i < nums.size(); i++) {
+
+            // If current number is negative,
+            // max becomes min and min becomes max
+            if (nums[i] < 0) {
+                swap(maxProd, minProd);
+            }
+
+            maxProd = max(nums[i], maxProd * nums[i]);
+
+            minProd = min(nums[i], minProd * nums[i]);
+
+            ans = max(ans, maxProd);
+        }
+
+        return ans;
+    }
+};
