9d4a2b8c75caa85eba42f94a18537e9efb60103d
Nilakshi Patil <patilnilakshi35@gmail.com>
2026-08-12 20:08:45 +0530

    Time: 9 ms (72.21%), Space: 41.6 MB (62.33%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty,
+                             vector<int>& profit,
+                             vector<int>& worker) {
+
+        vector<pair<int, int>> jobs;
+
+        for (int i = 0; i < difficulty.size(); i++) {
+            jobs.push_back({difficulty[i], profit[i]});
+        }
+
+        sort(jobs.begin(), jobs.end());
+        sort(worker.begin(), worker.end());
+
+        int ans = 0;
+        int j = 0;
+        int bestProfit = 0;
+
+        for (int ability : worker) {
+
+            while (j < jobs.size() && jobs[j].first <= ability) {
+                bestProfit = max(bestProfit, jobs[j].second);
+                j++;
+            }
+
+            ans += bestProfit;
+        }
+
+        return ans;
+    }
+};
