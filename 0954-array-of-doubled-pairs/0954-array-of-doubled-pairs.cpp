56b27b476750a72fa60c06c53f2eb4b11f5d2e45
Nilakshi Patil <patilnilakshi35@gmail.com>
2026-08-21 22:53:09 +0530

    Time: 42 ms (19.51%), Space: 61.8 MB (77.54%) - LeetHub

---
+++ 0954-array-of-doubled-pairs/0954-array-of-doubled-pairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canReorderDoubled(vector<int>& arr) {
+
+        unordered_map<int, int> freq;
+
+        for (int x : arr)
+            freq[x]++;
+
+        sort(arr.begin(), arr.end(),
+             [](int a, int b) {
+                 return abs(a) < abs(b);
+             });
+
+        for (int x : arr) {
+
+            if (freq[x] == 0)
+                continue;
+
+            if (freq[2 * x] == 0)
+                return false;
+
+            freq[x]--;
+            freq[2 * x]--;
+        }
+
+        return true;
+    }
+};
